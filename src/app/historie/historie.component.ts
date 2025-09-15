2ae3433872911165b72f320901bf6c04930fbb6c
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2025-09-15 20:08:26 +0200

    Historie überarbeitet

--- src/app/historie/historie.component.ts
+++ src/app/historie/historie.component.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SeasonsComponent } from '../history/seasons/seasons.component';
 import { MatchdaysComponent } from '../history/matchdays/matchdays.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
     selector: 'app-historie',
-    imports: [ SeasonsComponent, MatchdaysComponent, CommonModule, FormsModule],
+    imports: [ SeasonsComponent, MatchdaysComponent, CommonModule, FormsModule, MatButtonModule, MatCardModule],
     templateUrl: './historie.component.html',
     styleUrl: './historie.component.css'
 })
 export class HistorieComponent {
     selectedView: string = 'historisch'; // Standardauswahl
+      onAction(action: string) {
+    // Platzhalter: ersetze durch deine Logik (Routing, Dialog, Service-Aufruf etc.)
+    console.log('Button geklickt:', action);
+    // Beispiel: wenn du Routing willst:
+    // this.router.navigate(['/detail', action]);
+  }
 }
