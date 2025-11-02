681ee5007101bcacc6268b8a972382ab02e27c56
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2025-11-02 09:50:39 +0100

    Update menu card titles and improve admin component links

--- src/app/history/historie-admin/historie-admin.component.ts
+++ src/app/history/historie-admin/historie-admin.component.ts
@@ -9,7 +9,17 @@ import { UploadService } from '../../services/upload.service';
   styleUrl: './historie-admin.component.css'
 })
 export class HistorieAdminComponent {
-importStatus: string = 'Bereit zum Starten des Imports.';
+  abfragen: { name: string, url: string }[] = [
+    { name: "Datenbank verwalten", url: "http://localhost:8080/h2-console" },
+    { name: "Spieltage anzeigen", url: "http://localhost:8080/api/historie/spieltage/202223" },
+    { name: "Spieltagskader anzeigen", url: "http://localhost:8080/api/historie/spieltagskader?saison=202425&spiel=1" },
+    { name: "EV-Bälle", url: "http://localhost:8080/api/files/download?filePath=./Saison2526/EV-B%C3%A4lle/Saison2526_03_B%C3%A4lle.png" },
+    { name: "Spielzeiten mit Dokumenten", url: "http://localhost:8080/api/historie/documents/saisons" },
+    { name: "Torvideos", url: "http://localhost:8080/api/historie/documents?typ=Torvideos&saison=2526&spieltag=01" },
+    { name: "Dokumente", url: "http://localhost:8080/api/historie/documents?typ=Dokumente&saison=2526&spieltag=01" },
+    { name: "Einsätze eines Spielers", url: "http://localhost:8080/api/historie/einsaetze?nachname=Heinz&vorname=Merlin" }
+  ];
+  importStatus: string = 'Bereit zum Starten des Imports.';
   isImporting: boolean = false;
 
   constructor(private adminService: UploadService) { }
